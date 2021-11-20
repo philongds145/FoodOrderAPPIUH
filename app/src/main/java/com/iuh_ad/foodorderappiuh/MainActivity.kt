c80f8fb88e598263fbbe6c6ba9d988084a97b070
philongds145 <philongds145@gmail.com>
2021-11-20 20:17:28 +0700

    commitfirst

---
+++ app/src/main/java/com/iuh_ad/foodorderappiuh/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.iuh_ad.foodorderappiuh
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
